f59684b3499d842f22470595f38c6aba170c787f
zhang740 <zhang740@vmzclub.com>
2018-11-06 16:11:06 +0800

    update: set request timeout to 5s.

--- lib/util/getDataFromUrl.ts
+++ lib/util/getDataFromUrl.ts
@@ -2,7 +2,7 @@ import * as request from 'request';
 
 export async function getDataFromUrl(url: string) {
   return new Promise<string>((resolve, reject) => {
-    request(url, (error, response, body) => {
+    request(url, { timeout: 5000 }, (error, response, body) => {
       if (error) {
         console.warn('[Api-GenSDK] err', error);
         reject(error);
