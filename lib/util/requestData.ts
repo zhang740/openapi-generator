b7b0a5bf9d68b056cd79db4241fc667598c876fa
liunian <dengjij@gmail.com>
2020-06-05 22:02:49 +0800

    customize request timeout with env

--- lib/util/requestData.ts
+++ lib/util/requestData.ts
@@ -1,8 +1,10 @@
 import * as request from 'request';
 
+const timeout = process.env.requestTimeout ? (parseInt(process.env.requestTimeout, 10) || 5000) : 5000;
+
 export async function requestData(url: string) {
   return new Promise<string>((resolve, reject) => {
-    request(url, { timeout: 5000 }, (error, response, body) => {
+    request(url, { timeout: timeout }, (error, response, body) => {
       if (error) {
         console.warn('[GenSDK] err', error);
         reject(error);
